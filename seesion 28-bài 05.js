c9e94601d8dfaa0298a37bad4614737cbe04aa6f
quangduymanh <quangduymanh4@gmail.com>
2025-03-17 09:22:27 +0700

    upload 05

---
+++ seesion 28-bài 05.js
@@ -0,0 +1,14 @@
+const company = {
+    name: "RikkeiSoft",
+    location: "Hà Nội",
+    employees: [
+        { name: "Nguyễn Văn Luận", position: "Developer" },
+        { name: "Nguyễn Văn Hoàng", position: "Tester" },
+        { name: "Hoàng Nam Cao", position: "Manager" }
+
+    ]
+
+};
+console.log("công ty:",company.name);
+console.log("thành phố:",company.location);
+console.log("tên nhân viên:",company.employees);
